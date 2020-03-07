e4e97e4704e8afe1f012ebd898c313d35050cac8
etEthiopia <daginegussu@gmail.com>
2020-03-07 11:34:50 +0300

    Layout

--- App.js
+++ App.js
@@ -1,29 +1,41 @@
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  View,
-  Text
-  
-} from "react-native";
-
+import { StyleSheet, View, Text } from "react-native";
 
 export default function App() {
-  
-
   return (
     <View style={styles.container}>
-      
-     
-            <Text>Hello</Text>
-         
+      <View style={styles.result}></View>
+      <View style={styles.calculation}></View>
+      <View style={styles.buttons}>
+        <View style={styles.numbers}></View>
+        <View style={styles.operations}></View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#fff",
-    paddingVertical: 40,
-    paddingHorizontal: 15
+    flex: 1
+  },
+  result: {
+    flex: 2,
+    backgroundColor: "red"
+  },
+  calculation: {
+    flex: 1,
+    backgroundColor: "green"
+  },
+  buttons: {
+    flex: 6,
+    flexDirection: "row"
+  },
+  numbers: {
+    flex: 3,
+    backgroundColor: "yellow"
+  },
+  operations: {
+    flex: 1,
+    backgroundColor: "black"
   }
 });
